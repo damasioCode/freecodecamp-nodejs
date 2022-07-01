c43b48f8fb64fa939ca4bf3ee3c47c8197aaca92
Daniel Damasio <85068993+damasioCode@users.noreply.github.com>
2022-07-01 01:09:12 +0000

    Finish basic steps of express

--- myApp.js
+++ myApp.js
@@ -1,39 +1,59 @@
 let express = require('express');
 let app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config()
+process.env.TZ = 'America/Sao_Paulo'
+app.use(express.json());
+app.use('/public', express.static(__dirname + '/public'));
+app.use(
+  bodyParser.urlencoded({extended: false})
+);
+app.use( (req, res, next) => {
+  const {method, path, ip} = req;
+  console.log("I'm a middleware...");
+  console.log(`${method} ${path} - ${ip}`);
+
+  next();
+});
 
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
 
 
+app.get('/', (req, res) => {
+  const absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);
+});
 
+app.get('/json', (req, res) => {
+  if(process.env.MESSAGE_STYLE === "uppercase") {
+    return res.json({"message": "HELLO JSON"});
+  }
+  return res.json({"message": "Hello json"});
+});
+module.exports = app;
 
+app.get('/:word/echo', (req, res) => {
+  const { word } = req.params;
+  res.json({echo: word})
+});
 
-
-console.log('Hello World');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get('/name', (req, res) => {
+  const { first, last } = req.query;
+  res.json({ name: `${first} ${last}`})
+});
+app.post('/name', (req, res) => {
+  const { first, last } = req.body;
+  res.json({ name: `${first} ${last}`})
+});
